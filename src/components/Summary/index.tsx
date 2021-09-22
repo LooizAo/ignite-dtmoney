84b3274b7758e87d2da0754e7aa8e31f039f3451
Luiz Souza <looizao@protonmail.com>
2021-09-22 20:37:33 -0300

    creating Transactions Context and hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,3 +1,5 @@
+import { useTransactions } from '../../hooks/useTransactions';
+
 import incomeImg from '../../assets/income.svg';
 import expenseImg from '../../assets/expense.svg';
 import totalImg from '../../assets/total.svg';
@@ -5,6 +7,26 @@ import totalImg from '../../assets/total.svg';
 import { Container } from './styles';
 
 export function Summary() {
+  const { transactions } = useTransactions();
+
+  
+
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'deposit') {
+      acc.deposits += transaction.amount;
+      acc.total += transaction.amount;
+    } else {
+      acc.withdraw += transaction.amount;
+      acc.total -= transaction.amount;
+    }
+
+    return acc;
+  }, {
+    deposits: 0,
+    withdraw: 0,
+    total: 0
+  });
+
   return (
     <Container>
       <div>
@@ -12,21 +34,36 @@ export function Summary() {
           <p>Income</p>
           <img src={incomeImg} alt="Income" />
         </header>
-        <strong>$1000,00</strong>
+        <strong>
+          {new Intl.NumberFormat('en-US', {
+                  style: 'currency',
+                  currency: 'USD'
+          }).format(summary.deposits)}
+        </strong>
       </div>
       <div>
         <header>
           <p>Expenses</p>
           <img src={expenseImg} alt="Expenses" />
         </header>
-        <strong>$1000,00</strong>
+        <strong>
+          {new Intl.NumberFormat('en-US', {
+                  style: 'currency',
+                  currency: 'USD'
+          }).format(summary.withdraw)}
+        </strong>
       </div>
       <div className="highlight-background">
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Remaining" />
         </header>
-        <strong>$1000,00</strong>
+        <strong>
+          {new Intl.NumberFormat('en-US', {
+                  style: 'currency',
+                  currency: 'USD'
+          }).format(summary.total)}
+        </strong>
       </div>
     </Container>
   )
